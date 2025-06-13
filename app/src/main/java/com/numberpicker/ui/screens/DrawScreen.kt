fe178b3197894925b6b2fb3f031d462fe8c7e900
Bryan Fagundes <bryan.fagundes@crearesistemas.com.br>
2025-06-13 11:02:21 -0300

    First commit

---
+++ app/src/main/java/com/numberpicker/ui/screens/DrawScreen.kt
@@ -0,0 +1,24 @@
+package com.numberpicker.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DrawScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Home Screen!")
+        Button(onClick = { /* Navigate to another screen */ }) {
+            Text(text = "Go to Next Screen")
+        }
+    }
+}
